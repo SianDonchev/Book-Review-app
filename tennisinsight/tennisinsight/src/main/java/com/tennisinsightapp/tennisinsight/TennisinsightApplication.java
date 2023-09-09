3f2474033a2d1f54ed41161d601c6031352104d4
nii4eto <niadonceva@abv.bg>
2023-09-09 16:57:30 +0300

    Add data set for atp tournaments and matches for 2019

---
+++ tennisinsight/tennisinsight/src/main/java/com/tennisinsightapp/tennisinsight/TennisinsightApplication.java
@@ -0,0 +1,13 @@
+package com.tennisinsightapp.tennisinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisinsightApplication.class, args);
+	}
+
+}
---
+++ tennisinsight/tennisinsight/src/test/java/com/tennisinsightapp/tennisinsight/TennisinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisinsightapp.tennisinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
